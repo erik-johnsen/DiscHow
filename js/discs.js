8e6701f0562ee8ff8c376959ed1a4b2c19de27d9
Erik Johnsen <141843146+erik-johnsen@users.noreply.github.com>
2023-11-15 12:20:51 +0100

    Added 16 discs to "discs" variable

--- js/discs.js
+++ js/discs.js
@@ -56,15 +56,125 @@ const discs = [
     },
 	
 	{
-		"image": "./assets/Disc-img/disc-img_destroyer.jpeg",
-        "name": "Destroyer",
-        "brand": "innova",
-        "speed": "12",
-        "glide": "5",
-        "turn": "-1",
-        "fade": "3",
+		"image": "./assets/Disc-img/disc-img_envy.jpeg",
+        "name": "Envy",
+        "brand": "mvp",
+        "speed": "3",
+        "glide": "3",
+        "turn": "0",
+        "fade": "2",
+		"disc-type": "putter"
+    },
+
+    {
+		"image": "./assets/Disc-img/disc-img_falk.png",
+        "name": "Falk",
+        "brand": "kastaplast",
+        "speed": "9",
+        "glide": "6",
+        "turn": "-2",
+        "fade": "1",
 		"disc-type": "distance"
-    }
+    },      
+
+    {
+		"image": "./assets/Disc-img/disc-img_fd.jpeg",
+        "name": "FD",
+        "brand": "discmania",
+        "speed": "7",
+        "glide": "6",
+        "turn": "0",
+        "fade": "1",
+		"disc-type": "fairway"
+    }, 
+    
+    {
+		"image": "./assets/Disc-img/disc-img_glitch.jpeg",
+        "name": "Glitch",
+        "brand": "mvp",
+        "speed": "1",
+        "glide": "7",
+        "turn": "0",
+        "fade": "0",
+		"disc-type": "putter"
+    },
+
+    {
+		"image": "./assets/Disc-img/disc-img_kaxe.jpeg",
+        "name": "Kaxe",
+        "brand": "kastaplast",
+        "speed": "6",
+        "glide": "4",
+        "turn": "0",
+        "fade": "3",
+		"disc-type": "midrange"
+    },
+
+    {
+		"image": "./assets/Disc-img/disc-img_leopard.jpeg",
+        "name": "Leopard",
+        "brand": "innova",
+        "speed": "6",
+        "glide": "5",
+        "turn": "-2",
+        "fade": "1",
+		"disc-type": "fairway"
+    },
+
+    {
+		"image": "./assets/Disc-img/disc-img_link.jpeg",
+        "name": "Link",
+        "brand": "discmania",
+        "speed": "2",
+        "glide": "3",
+        "turn": "0",
+        "fade": "1",
+		"disc-type": "putter"
+    },
+
+    {
+		"image": "./assets/Disc-img/disc-img_penrose.jpeg",
+        "name": "Penrose",
+        "brand": "ev-7",
+        "speed": "2",
+        "glide": "4",
+        "turn": "0",
+        "fade": "2",
+		"disc-type": "putter"
+    },
+
+    {
+		"image": "./assets/Disc-img/disc-img_roc.jpeg",
+        "name": "Roc",
+        "brand": "innova",
+        "speed": "4",
+        "glide": "4",
+        "turn": "0",
+        "fade": "3",
+		"disc-type": "midrange"
+    },
+
+    {
+		"image": "./assets/Disc-img/disc-img_teebird.jpeg",
+        "name": "Teebird",
+        "brand": "innova",
+        "speed": "7",
+        "glide": "5",
+        "turn": "0",
+        "fade": "2",
+		"disc-type": "fairway"
+    },
+
+    {
+		"image": "./assets/Disc-img/disc-img_zone.jpeg",
+        "name": "Zone",
+        "brand": "discraft",
+        "speed": "4",
+        "glide": "3",
+        "turn": "0",
+        "fade": "3",
+		"disc-type": "putter"
+    },
 ];
 
 export { discs }
